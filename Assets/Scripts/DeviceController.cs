39f68a91b2f9815a4ccc09d4438779e3aa9bb368
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-03-29 13:08:14 +0100

    🐛 Two gamepad

---
+++ Assets/Scripts/DeviceController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class DeviceController : MonoBehaviour
+{
+    private PlayerInput m_PlayerInput;
+
+    void Awake()
+    {
+        m_PlayerInput = GetComponent<PlayerInput>();
+    }
+
+    void Start()
+    {
+        if (m_PlayerInput.user.pairedDevices.Count > 1)
+        {
+            m_PlayerInput.user.UnpairDevice(m_PlayerInput.user.pairedDevices[1]);
+        }
+    }
+}
